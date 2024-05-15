4d773dcd3e19ea8b163d717d074a9a451eb2caeb
Mattlpt <matthieu.lpt56@gmail.com>
2024-05-15 11:03:43 +0200

    init

---
+++ src/main/java/com/lycoris/lycorisbackend/LycorisBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lycoris.lycorisbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LycorisBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LycorisBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lycoris/lycorisbackend/LycorisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lycoris.lycorisbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LycorisBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
